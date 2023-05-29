6faef2142b3220182e77a7fdebee8e40a142f403
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2023-05-29 18:33:45 +0200

    many things made

--- js/index.js
+++ js/index.js
@@ -2,30 +2,19 @@ import { hamburgerMenu } from "../js/contants/hamburger.js";
 
 hamburgerMenu();
 
-// function scrollToSection(event) {
-//   event.preventDefault();
-
-//   const sectionId = event.target.getAttribute("href");
-//   const section = document.querySelector(sectionId);
-
-//   if (section) {
-//     section.scrollIntoView({
-//       behavior: "smooth",
-//       block: "start",
-//       inline: "nearest",
-//       duration: 2000,
-//     });
+// document.querySelectorAll(".nav-links").forEach((link) => {
+//   if (link.href === window.location.href) {
+//     link.setAttribute("aria-current", "location");
 //   }
-// }
-
-// const scrollLinks = document.querySelectorAll(".scroll-link");
-// scrollLinks.forEach(function (link) {
-//   link.addEventListener("click", scrollToSection);
 // });
 
-document.querySelectorAll(".nav-links").forEach((link) => {
-  if (link.href === window.location.href) {
-    link.setAttribute("aria-current", "location");
-  }
-  console.log("hi");
+document.addEventListener("DOMContentLoaded", () => {
+  const currentUrl = window.location.href;
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach((link) => {
+    if (link.href === currentUrl) {
+      link.setAttribute("aria-current", "page");
+    }
+  });
 });
